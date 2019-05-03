b0f371564a592e9dd600f5865b157bc9b040f996
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-05-03 11:22:53 +0100

    feat(signin): super-admin signin
    - add login page
    - add dashboard route and component
    - add auth service
    
    [Finishes #164072736]

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: AuthComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
