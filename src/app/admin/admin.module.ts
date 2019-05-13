add4968b5110e14a455d114337ca40163d6a9463
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-05-13 13:12:27 +0100

    feat(articles): create shared modules

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { SharedModule } from '../shared.module';
 
 @NgModule({
   imports: [
     AdminRoutingModule,
+    SharedModule,
   ],
   exports: [],
   declarations: [
